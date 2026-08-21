b48e0b032431b155c78248e9b5ee19982c8b6f28
kaboozyako <kaboozyako@gmail.com>
2026-08-21 19:41:08 +0300

    fix banner

--- packages/web/app/page.tsx
+++ packages/web/app/page.tsx
@@ -29,7 +29,7 @@ export default function Page() {
 
       <NetworkBanner />
 
-      <section className="mx-auto w-full max-w-md px-4 pt-6 pb-2 flex justify-center">
+      <section className="mx-auto w-full max-w-md px-4 pt-6 -mb-16 flex justify-center">
         <img 
           src="/banner.png" 
           alt="SMALL STEPS TO BIG WINS" 
